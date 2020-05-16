5da2f0648738616d1b7f969caff8673681a47fd6
Begemotas <cheminiselementas@gmail.com>
2020-05-16 21:37:24 +0300

    fetch items actions added

---
+++ src/CoffeeShop/store/actions/fetchItems.js
@@ -0,0 +1,27 @@
+import * as actionTypes from './actionTypes';
+
+export const fetchItemsStart=()=>{
+    return{
+        type: actionTypes.FETCH_ITEMS_START
+    }
+}
+
+export const fetchItemsSuccess=()=>{
+    return{
+        type: actionTypes.FETCH_ITEMS_SUCCESS
+
+    }
+}
+
+export const fetchItemsFail=()=>{
+    return{
+        type:actionTypes.FETCH_ITEMS_FAIL
+
+    }
+}
+
+export const fetchItems=()=>{
+    return dispatch=>{
+
+    }
+}
