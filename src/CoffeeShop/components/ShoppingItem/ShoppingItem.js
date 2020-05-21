3282d6a68b01887dabb3a39f36073b39b7ea1472
Begemotas <cheminiselementas@gmail.com>
2020-05-21 18:59:08 +0300

    shopping items section version1 redone usinf sass and css modules

--- src/CoffeeShop/components/ShoppingItem/ShoppingItem.js
+++ src/CoffeeShop/components/ShoppingItem/ShoppingItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
-import classes from './ShoppingItem.module.css';
+import classes from './ShoppingItem.scss';
+import addButtonClass from './ShoppingItemButton.module.css';
 import ShoppingItemImg from '../ShoppingItemImg/ShoppingItemImg';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline';
@@ -8,18 +9,20 @@ import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline';
 const shoppingItem=(props)=>{
    // let itemLogos=[{tangerineLogo}, {appleLogo}, {kiwiLogo} ]
    // itemLogos.map();
+
+
         return (
-            <div className={classes.Item}>
-                <div className={classes.Logo}>
-                    <ShoppingItemImg name={props.name}/>
-                </div>
-                <h1 className={classes.ItemName}> {props.name} </h1>
-                <div className={classes.ItemDescription}>{props.description}</div>
-                <h4 className={classes.ItemPrice}>€/kg: {props.price}</h4>
-                <h3 className={classes.ItemPrice}>kg: {props.count}</h3>
-                <div className={classes.Buttons}>
-                <Button onClick={props.removeItem} className={classes.Button}><RemoveCircleOutlineIcon className={classes.RemoveIcon}/></Button>
-                <Button onClick={props.addItem} className={classes.Button} ><AddCircleOutlineIcon className={classes.AddIcon}/></Button>
+            <div class="Item">
+                   <div class="Logo">
+                        <ShoppingItemImg name={props.name}/>
+                    </div>
+                <h1 class="ItemName"> {props.name} </h1>
+                <div class="ItemDescription">{props.description}</div>
+                <h4 class="ItemPrice">€/kg: {props.price}</h4>
+                <h3 class="ItemPrice">kg: {props.count}</h3>
+                <div class="Buttons">
+                <Button onClick={props.removeItem} className={classes.Button}><RemoveCircleOutlineIcon /></Button>
+                <Button onClick={props.addItem} className={classes.Button} ><AddCircleOutlineIcon className={addButtonClass.AddIcon}/></Button>
                 </div>
             </div>
         )
