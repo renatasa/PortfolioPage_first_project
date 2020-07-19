8bb30ff648409c0255e809628e4cca790194889b
Begemotas <cheminiselementas@gmail.com>
2020-07-19 16:03:17 +0300

    aboutMe component made responsive

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -7,26 +7,24 @@ import cssSVG from './logos/cssSVG.svg';
 import javascriptSVG from './logos/javascriptSVG.svg';
 import sassSVG from './logos/sassSVG.svg';
 import gitSVG from './logos/gitSVG.svg';
-;
+import githubSVG from './logos/githubSVG.svg';
+
  const AboutMe =(props)=>  {
+
         return (
             <div class="aboutMe">
                
-            <div class="aboutMe__text">
-                <h2><li>Frontend developer with main interest in React</li></h2>
-                <h3><li>This is my portfolio website</li></h3>
+            <div >
+                <h2>Frontend developer with main interest in React</h2>
+                <h3>Thank you for visiting my website!</h3>
                 {/* <div>
                     CSS SASS Javascript React Redux Git
                 </div> */}
             </div>    
 
-            <div class="aboutMe__logos" >
-                <img src={cssSVG} alt={'reduxSVG'} class="aboutMe__logos-item-css"/>
-                <img src={javascriptSVG} alt={'reduxSVG'} class="aboutMe__logos-item"/>
-                <img src={sassSVG} alt={'reduxSVG'} class="aboutMe__logos-item"/>
-                <img src={reactSVG} alt={'reduxSVG'} class="aboutMe__logos-item"/>
-                <img src={reduxSVG} alt={'reduxSVG'} class="aboutMe__logos-item"/>
-                <img src={gitSVG} alt={'reduxSVG'} class="aboutMe__logos-item"/>
+            <div class="aboutMe__github">
+                    <img src={githubSVG} alt={'githubSVG'} class="aboutMe__logos-item   aboutMe__logos-item-git"/>
+                    <div class="aboutMe__projects__buttons-item aboutMe__projects__buttons-item-git"> <a href="https://github.com/renatasa">My GitHub</a></div> 
             </div>
 
             <div class="aboutMe__projects">
@@ -36,6 +34,17 @@ import gitSVG from './logos/gitSVG.svg';
                     <div class="aboutMe__projects__buttons-item">COVID statistics</div>
                 </div>
             </div>   
+
+
+            <div class="aboutMe__logos" >
+                <img src={cssSVG} alt={'cssSVG'} class="aboutMe__logos-item   aboutMe__logos-css"/>
+                <img src={javascriptSVG} alt={'javascriptSVG'} class="aboutMe__logos-item"/>
+                <img src={sassSVG} alt={'sassSVG'} class="aboutMe__logos-item"/>
+                <img src={reactSVG} alt={'reactSVG'} class="aboutMe__logos-item"/>
+                <img src={reduxSVG} alt={'reduxSVG'} class="aboutMe__logos-item"/>
+                <img src={gitSVG} alt={'gitSVG'} class="aboutMe__logos-item"/>
+            </div>
+
             </div>
 
 
